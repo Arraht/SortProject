da4cac50ffa5745428c31cad4316812de892444a
User <yanixi@yandex.ru>
2026-01-22 21:43:27 +0300

    Создать классы Entity и написать свой компаратор к ним

--- src/main/java/project/sort/Main.java
+++ src/main/java/project/sort/Main.java
@@ -1,19 +1,11 @@
 package project.sort;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Person> entities = new ArrayList<>();
-        entities.add(new Person(1L, "Alice", 3));
-        entities.add(new Person(2L, "Bob", 1));
-        entities.add(new Person(3L, "Petr", 2));
-        entities.add(new Person(4L, "Charlie", 4));
+        PersonSorter sorter = new PersonSorter();
 
-        entities.sort(new EntityComparator());
+        System.out.println();
+        sorter.sortAndPrintPersons();
 
-        for (Person entity : entities) {
-            System.out.println(entity);
-        }
     }
 }
---
+++ src/main/java/project/sort/sortAndPrintPersons.java
@@ -0,0 +1,20 @@
+package project.sort;
+
+import java.util.ArrayList;
+
+public class PersonSorter {  // ← класс с большой буквы
+
+    public void sortAndPrintPersons() {  // ← метод с маленькой буквы
+        ArrayList<Person> entities = new ArrayList<>();
+        entities.add(new Person(1L, "Alice", 3));
+        entities.add(new Person(2L, "Bob", 1));
+        entities.add(new Person(3L, "Petr", 2));
+        entities.add(new Person(4L, "Charlie", 4));
+
+        entities.sort(new EntityComparator());
+
+        for (Person entity : entities) {
+            System.out.println(entity);
+        }
+    }
+}
