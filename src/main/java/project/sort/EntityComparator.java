021424608ab83ecfbc08e3f126204bd5dd153a99
User <yanixi@yandex.ru>
2026-01-22 20:19:56 +0300

    Создать классы Entity и написать свой компаратор к ним

--- src/main/java/project/sort/EntityComparator.java
+++ src/main/java/project/sort/EntityComparator.java
@@ -2,21 +2,19 @@ package project.sort;
 
 import java.util.Comparator;
 
-public class EntityComparator implements Comparator<Entity> {
+public class EntityComparator implements Comparator<Person> {
 
     @Override
-    public int compare(Entity e1, Entity e2){
-        if (e1.getPriority() != e2.getPriority()){
+    public int compare(Person e1, Person e2) {
+        if (e1.getPriority() != e2.getPriority()) {
             return Integer.compare(e2.getPriority(), e1.getPriority());
         }
 
         int nameComp = e1.getName().compareTo(e2.getName());
-        if (nameComp != 0){
+        if (nameComp != 0) {
             return nameComp;
         }
 
-        return Integer.compare(e1.getId(), e2.getId());
+        return Long.compare(e1.getId(), e2.getId());
     }
-
-
 }
