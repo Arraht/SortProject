31efd49653e869e3fbe6e77cdaa2d1ee3bcba17f
Arraht <igor_161995@mail.ru>
2026-01-23 01:04:16 +0600

    поправил мр

--- src/main/java/project/sort/Person.java
+++ src/main/java/project/sort/Person.java
@@ -1,16 +1,32 @@
 package project.sort;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+import java.util.Objects;
+
+@Data
 @AllArgsConstructor
-@ToString
+@NoArgsConstructor
 public class Person {
     private Long id;
     private String name;
     private int priority;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return priority == person.priority && Objects.equals(id, person.id) && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, priority);
+    }
+
+    public String toString() {
+        return "id: " +  this.id + "; name: " + this.name + "; priority: " + this.priority;
+    }
 }
--- src/main/java/project/sort/PersonSorter.java
+++ src/main/java/project/sort/PersonSorter.java
@@ -2,9 +2,8 @@ package project.sort;
 
 import java.util.ArrayList;
 
-public class PersonSorter {  // ← класс с большой буквы
-
-    public void sortAndPrintPersons() {  // ← метод с маленькой буквы
+public class PersonSorter {
+    public void sortAndPrintPersons() {
         ArrayList<Person> entities = new ArrayList<>();
         entities.add(new Person(1L, "Alice", 3));
         entities.add(new Person(2L, "Bob", 1));
